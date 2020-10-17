30c1d675aec6c7d77661cd9598c1129db9f706ea
Glitch (frogs-in-space) <none>
2020-10-17 20:24:11 +0000

    🌁🌲 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,22 @@
+// server.js
+// where your node app starts
+
+// we've started you off with Express (https://expressjs.com/)
+// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
+
+app.use('/css',express.static(__dirname + '/css'));
+app.use('/js',express.static(__dirname + '/js'));
+app.use('/assets',express.static(__dirname + '/assets'));
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname+'/views/index.html');
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
