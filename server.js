786af17e30b8863ca69f8c351091f95cde5d9b92
Glitch (frogs-in-space) <none>
2020-10-17 20:42:46 +0000

    💼🌉 Updated with Glitch

--- server.js
+++ server.js
@@ -13,10 +13,38 @@ app.use('/js',express.static(__dirname + '/js'));
 app.use('/assets',express.static(__dirname + '/assets'));
 
 app.get('/',function(req,res){
-  res.sendFile(__dirname+'/views/index.html');
+  res.sendFile(__dirname+'/index.html');
 });
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+server.lastPlayderID = 0; // Keep track of the last id assigned to a new player
+
+io.on('connection',function(socket){
+    socket.on('newplayer',function(){
+        socket.player = {
+            id: server.lastPlayderID++,
+            x: randomInt(100,400),
+            y: randomInt(100,400)
+        };
+        socket.emit('allplayers',getAllPlayers());
+        socket.broadcast.emit('newplayer',socket.player);
+    });
+});
+
+function getAllPlayers(){
+    var players = [];
+    Object.keys(io.sockets.connected).forEach(function(socketID){
+        var player = io.sockets.connected[socketID].player;
+        if(player) players.push(player);
+    });
+    return players;
+}
+
+function randomInt (low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
+}
+
